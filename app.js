3cf78245a453ee81018be33ee4d25d2daef26aec
sgwicks <samgwicks@gmail.com>
2020-08-05 11:46:14 +0100

    Add first test case (placeholder)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.status(200).send('Hello world')
 })
 
 app.listen(2020)
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../app')
+
+describe('app', () => {
+    test('Sends a 200 response', () => {
+        request(app)
+            .get('/')
+            .expect(200)
+            .then((res) => {
+                expect(res.text).toBe('Hello world')
+            })
+    })
+})
