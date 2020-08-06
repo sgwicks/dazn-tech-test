9effd40b71cf06168b82872e6a60d9c7099f1d0f
sgwicks <samgwicks@gmail.com>
2020-08-06 14:02:20 +0100

    Install CORS package, update app.js homepage

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const cors = require('cors')
 const app = require('express')()
 const { PORT = 9090 } = process.env;
+const fs = require('fs')
 
 app.use(cors())
 
 const apiRouter = require('./routes/apiRouter')
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello world')
+    res.status(200).send('https://github.com/sgwicks/dazn-tech-test')
 })
 
 app.use('/api', apiRouter)
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+const ENV = process.env.NODE_ENV || 'development';
 const { DB_URL } = process.env;
 const user = require('./user');
 
@@ -33,4 +34,4 @@ const dbConfig = {
  
 };
 
-module.exports = dbConfig
+module.exports = dbConfig[ENV]
